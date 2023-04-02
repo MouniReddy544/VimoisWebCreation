9f755ca33cdbbc2f18fcf1840d9fcf4fc764fb3b
Mounika Nagireddy <mounikanagireddy@MacBook-Pro-3.attlocal.net>
2023-04-02 00:25:54 -0500

    adding project01

---
+++ project01/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/VimoisLogo.png" width="200" height="100">
+  <HelloWorld msg="Welcome to vimois llc"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
